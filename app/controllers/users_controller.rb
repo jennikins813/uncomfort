8bab7eb826acd7807b2c899f330f04aa2b610b14
shmems11 <shmems11@gmail.com>
2014-05-12 15:48:36 -0400

    added new action create action strong params and a partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,17 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new user_params
+    if @user.save
+      auto_login(@user)
+      redirect_to user_url, :notice => "Signed Up!"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +26,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:name, :image, :bio, :email, :password, :password_confirmation)
   end
 end
