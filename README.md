8af4284f7052e58bb586a1e40c13057d79144e34
shmems11 <shmems11@gmail.com>
2014-05-12 14:36:18 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+project_uncomfort
+=================
+
+A Web App to Help People Break Out of Their Comfort Zone
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Uncomfort::Application.config.secret_key_base = '2d868e9dc62fe8c028e58064e4270798e311c64e90a6d5dd1d7a737c11e7b5798f387b2ba8dd83942843467aeb4c97027fdaaa5dae945d8765f8066b46f9b449'
