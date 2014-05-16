b9ace9c74277578601981abff4c25d8978af74df
jennikins813 <jennikins813@yahoo.com>
2014-05-16 12:17:00 -0400

    Added/updated views for profiles/users/application controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def current_user
+  	@current_user ||= User.find(user_session[:user_id]) if user_session[:user_id]
+  end
+
+  helper_method :current_user
+
+  def ensure_logged_in
+  	unless current_user
+  		flash[:alert] = "Please log in"
+  		redirect_to new_user_session_path
+  	end
+  end
   
   
 end
