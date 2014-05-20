506542cb445f6f02d155571c2131d2e5777ae039
jennikins813 <jennikins813@yahoo.com>
2014-05-20 06:21:05 -0400

    Added inverse friendship association for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ActiveRecord::Base
   has_one :result
   has_many :friendships
   has_many :friends, :through => :friendships  
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user  
+
   #has_one :profile
   #accepts_nested_attributes_for :profile
   mount_uploader :image, ImageUploader
