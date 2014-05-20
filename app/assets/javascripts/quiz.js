bc264a46a224caa1507903e81142ff284d74de70
MemoryB <mb_macdonald@yahoo.ca>
2014-05-20 19:53:58 -0400

    changed quiz buttons

--- app/assets/javascripts/quiz.js
+++ app/assets/javascripts/quiz.js
@@ -1,36 +1,43 @@
 $(document).ready(function () {
-    
-    answers = new Object();
-    $('.answers').change(function () {
-        var answer = ($(this).attr('value'));
-        var question = ($(this).attr('name'));
-        answers[question] = answer;
-    });
 
-    var totalQuestions = $('.question').size();
-    var currentQuestion = 0;
-    $questions = $('.question');
-    $questions.hide();
-    $($questions.get(currentQuestion)).fadeIn();
+  $('#results').hide();
 
-    function sum_values() {
-        var the_sum = 0;
-        for (question in answers) {
-            the_sum = the_sum + parseInt(answers[question]);
-        }
-        return the_sum;
-    }
 
-    $('#next').click(function () {
-        $($questions.get(currentQuestion)).fadeOut(function () {
-            currentQuestion = currentQuestion + 1;
-            if (currentQuestion === totalQuestions) {
-                var result = sum_values();
-                $('#result_score').val(result);
-            } else {
-                $($questions.get(currentQuestion)).fadeIn();
-            }
-        });
-    });
+answers = new Object();
+$('.answers').change(function () {
+    var answer = ($(this).attr('value'));
+    var question = ($(this).attr('name'));
+    answers[question] = answer;
 });
 
+var totalQuestions = $('.question').size();
+var currentQuestion = 0;
+$questions = $('.question');
+$questions.hide();
+$($questions.get(currentQuestion)).fadeIn();
+
+function sum_values() {
+    var the_sum = 0;
+    for (question in answers) {
+        the_sum = the_sum + parseInt(answers[question]);
+    }
+    return the_sum;
+}
+
+$('#next').click(function () {
+    $($questions.get(currentQuestion)).fadeOut(function () {
+        currentQuestion = currentQuestion + 1;
+        if (currentQuestion === totalQuestions ) {
+            var result = sum_values();
+            $('#result_score').val(result);
+            $('#results').show() && $('#next').hide();
+        } else {
+            $($questions.get(currentQuestion)).fadeIn();
+        
+        }
+    });
+
+         
+});
+
+});
