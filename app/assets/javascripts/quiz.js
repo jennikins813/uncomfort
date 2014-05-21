ecd7ea7c4b7643905f133437bb295d4a21fba3b2
MemoryB <mb_macdonald@yahoo.ca>
2014-05-21 12:55:09 -0400

    added remaining images to quiz

--- app/assets/javascripts/quiz.js
+++ app/assets/javascripts/quiz.js
@@ -3,20 +3,20 @@ $(document).ready(function () {
   $('#results').hide();
 
 
-answers = new Object();
-$('.answers').change(function () {
+  answers = new Object();
+  $('.answers').change(function () {
     var answer = ($(this).attr('value'));
     var question = ($(this).attr('name'));
     answers[question] = answer;
 });
 
-var totalQuestions = $('.question').size();
-var currentQuestion = 0;
-$questions = $('.question');
-$questions.hide();
-$($questions.get(currentQuestion)).fadeIn();
+  var totalQuestions = $('.question').size();
+  var currentQuestion = 0;
+  $questions = $('.question');
+  $questions.hide();
+  $($questions.get(currentQuestion)).fadeIn();
 
-function sum_values() {
+  function sum_values() {
     var the_sum = 0;
     for (question in answers) {
         the_sum = the_sum + parseInt(answers[question]);
@@ -30,14 +30,15 @@ $('#next').click(function () {
         if (currentQuestion === totalQuestions ) {
             var result = sum_values();
             $('#result_score').val(result);
-            $('#results').show() && $('#next').hide();
+            $('#results').show() && $('#next').hide();  
+
         } else {
             $($questions.get(currentQuestion)).fadeIn();
-        
+            
         }
     });
 
-         
+    
 });
 
 });
