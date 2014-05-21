56c8282a10ea832df193c1c8816cb6f92bab7cde
jennikins813 <jennikins813@yahoo.com>
2014-05-21 09:52:46 -0400

    Create comment model and association with user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140520190002) do
+ActiveRecord::Schema.define(version: 20140521134833) do
 
   create_table "answers", force: true do |t|
     t.integer  "question_id"
     t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.text     "comment"
+    t.integer  "user_id"
+    t.integer  "tag_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
