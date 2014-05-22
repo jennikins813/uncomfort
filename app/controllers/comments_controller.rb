b10a72feb0105092d8d9f2778fe65a70427f0df6
MemoryB <mb_macdonald@yahoo.ca>
2014-05-22 14:54:28 -0400

    fixed owner of comment when comments are created

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,9 +9,22 @@ class CommentsController < ApplicationController
     @user = User.find(params[:id])
   end
 
+
+
+  # Parameters: {"utf8"=>"✓", "authenticity_token"=>"1FywcPVAOJULgNgyPUwR6QRtGVPBIJjPyk6aCQ+YGJA=", "comment"=>{"comment"=>"Hi Bunny!\r\n"}, "commit"=>"Create Comment", "user_id"=>"2"}
+
+  # This is what's happening, from above.
+  # params[:utf8]
+  # params[:authenticity_token]
+  # params[:user_id]  # WRONG
+
+  # params[:comment][:comment]
+  # params[:comment][:user_id]  #CORRECT
+  # params[:comment][:tag_id]
+
   def create
     @comment = @user.comments.build(comment_params)
-    @comment.user_id = current_user.id
+    @comment.commenter_id = current_user.id   # this is the person that is making the comment, ie. the logged in person.
 
     if @comment.save
       redirect_to users_path, notice: "Comment saved!"
@@ -29,6 +42,7 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:comment, :user_id, :tag_id) #or :tag_list??
   end
+
 
   def load_user
     @user = User.find(params[:user_id])
