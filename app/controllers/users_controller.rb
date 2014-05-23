1eec41890cd4dab7fba8a9c34aa45012dc1dcb91
jennikins813 <jennikins813@yahoo.com>
2014-05-23 15:16:39 -0400

    Adjusting layout and other adjustments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       auto_login(@user)
-      redirect_to root_path, :notice => "Signed Up!" 
+      redirect_to root_path, :notice => "Welcome to Uncomfort Yourself!" 
     else
       render :new
     end
